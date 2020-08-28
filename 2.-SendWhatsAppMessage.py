52ba72de1eb95f55327523d36735ed84e8492bb1
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2020-08-28 15:52:58 -0600

    Escribir mensaje en el cuadro de texto de un mensaje para WhatsApp

---
+++ 2.-SendWhatsAppMessage.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Autor : gustavo_exe
+
+#Pasos para que funcione
+
+#Paso 1:
+#Instalar: pip install pywhatkit desde el terminal
+
+#Codificando
+import pywhatkit as whatsapp
+
+#Tu navegador se abrira para enviar el mensaje
+whatsapp.sendwhatmsg("+'Coloca el destnatario'","Mensaje",13,24)
+
